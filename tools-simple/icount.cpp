e94734ae1f3cb900a0e87e4e771ff9ad792e6ccb
Alexis Engelke <engelke@in.tum.de>
2021-04-11 16:46:58 +0200

    tools/icount: Actually print result

--- tools-simple/icount.cpp
+++ tools-simple/icount.cpp
@@ -14,11 +14,58 @@ namespace {
 class Instrumenter {
 private:
     llvm::Function* instr_marker;
+    llvm::Function* syscall_fn;
+    llvm::Function* wrap_fn; // wrap syscall
+
+    void InitSyscallWrapper(llvm::Module* mod) {
+        syscall_fn = mod->getFunction("syscall");
+
+        llvm::IRBuilder<> irb(mod->getContext());
+        auto dprintf_ty = llvm::FunctionType::get(irb.getVoidTy(), true);
+        llvm::Function* dprintf_fn = llvm::Function::Create(dprintf_ty,
+                                            llvm::GlobalValue::ExternalLinkage,
+                                            "dprintf", mod);
+
+        wrap_fn = llvm::Function::Create(syscall_fn->getFunctionType(),
+                                         llvm::GlobalValue::ExternalLinkage,
+                                         "syscall_wrap", mod);
+        llvm::BasicBlock* bb1 = llvm::BasicBlock::Create(mod->getContext(), "",
+                                                         wrap_fn);
+        llvm::BasicBlock* bb2 = llvm::BasicBlock::Create(mod->getContext(), "",
+                                                         wrap_fn);
+        llvm::BasicBlock* bb3 = llvm::BasicBlock::Create(mod->getContext(), "",
+                                                         wrap_fn);
+
+        irb.SetInsertPoint(bb1);
+        llvm::Value* sptr = &wrap_fn->arg_begin()[0];
+        unsigned sptr_as = sptr->getType()->getPointerAddressSpace();
+        llvm::Type* i64p_sptr = irb.getInt64Ty()->getPointerTo(sptr_as);
+        llvm::Value* rax_ptr = irb.CreateConstGEP1_64(sptr, 0x8);
+        rax_ptr = irb.CreatePointerCast(rax_ptr, i64p_sptr);
+        llvm::Value* rax = irb.CreateLoad(irb.getInt64Ty(), rax_ptr);
+        // TODO: non-x86-64
+        llvm::Value* is_exit = irb.CreateICmpEQ(rax, irb.getInt64(231));
+        irb.CreateCondBr(is_exit, bb2, bb3);
+
+        irb.SetInsertPoint(bb2);
+        llvm::Value* count_ptr = irb.CreateConstGEP1_64(sptr, -0x40);
+        count_ptr = irb.CreatePointerCast(count_ptr, i64p_sptr);
+        llvm::Value* count_ld = irb.CreateLoad(count_ptr);
+        const char* fmt_str = "Instruction count: 0x%lx\n";
+        llvm::Constant* syscall_pre_fmt = irb.CreateGlobalStringPtr(fmt_str);
+        irb.CreateCall(dprintf_fn, {irb.getInt64(2), syscall_pre_fmt, count_ld});
+        irb.CreateBr(bb3);
+
+        irb.SetInsertPoint(bb3);
+        irb.CreateCall(syscall_fn, {sptr});
+        irb.CreateRetVoid();
+    }
 
 public:
     Instrumenter(const char* config, llvm::Module* mod,
                  InstrewDesc* out_desc) {
         instr_marker = mod->getFunction("instrew_instr_marker");
+        InitSyscallWrapper(mod);
 
         // Fields instrument and finalize are filled out below.
         out_desc->magic = 0xAEDB1000;
@@ -60,6 +107,8 @@ public:
                     if (call->getCalledFunction() == instr_marker) {
                         instr_count++;
                         call_sites.emplace_back(call);
+                    } else if (call->getCalledFunction() == syscall_fn) {
+                        call->setCalledFunction(wrap_fn);
                     }
                 }
             }
